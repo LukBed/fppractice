acf937fc52d12f5006bcca54a0b12f21d7379adb
Lukasz Bednorz <lbednorz.sag@gmail.com>
2020-12-28 21:48:28 +0100

    improvement count characters

--- src/main/scala/pl/snipersoft/fppractice/strings/StringUtils.scala
+++ src/main/scala/pl/snipersoft/fppractice/strings/StringUtils.scala
@@ -4,24 +4,26 @@ import scala.annotation.tailrec
 import scala.language.postfixOps
 
 object StringUtils {
+
   implicit class RichString(s: String) {
     def countCharacters: Map[Char, Int] = {
       @tailrec
       def helper(value: String, accumulator: Map[Char, Int]): Map[Char, Int] = {
-        if (value.length == 0) accumulator
-        else {
-          val firstChar = value.charAt(0)
-          val countOfFirstChar = accumulator.getOrElse(firstChar, 0) + 1
-          val newAccumulator = accumulator + (firstChar -> countOfFirstChar)
-          if (value.length == 1) {
-            newAccumulator
-          } else {
-            helper(value.substring(1), newAccumulator)
-          }
-        }
-      }
+        if (value.headOption.isEmpty) return accumulator
 
-      helper(s, Map.empty)
+        val firstChar = value.head
+        val countOfFirstChar = accumulator.getOrElse(firstChar, 0) + 1
+        val newAccumulator = accumulator + (firstChar -> countOfFirstChar)
+
+        value.tail match {
+          case "" => newAccumulator
+          case _ => helper(value.substring(1), newAccumulator)
+        }
     }
+
+    helper(s, Map.empty)
   }
+
+}
+
 }
