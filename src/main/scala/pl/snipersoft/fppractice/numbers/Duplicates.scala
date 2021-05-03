2bd8fdc5858c82c075668b24fe235074b496e0b2
Lukasz Bednorz <lbednorz.sag@gmail.com>
2021-05-03 19:22:41 +0200

    optimal duplicates numbers by Daniel

--- src/main/scala/pl/snipersoft/fppractice/numbers/Duplicates.scala
+++ src/main/scala/pl/snipersoft/fppractice/numbers/Duplicates.scala
@@ -1,24 +1,18 @@
 package pl.snipersoft.fppractice.numbers
 
-import scala.annotation.tailrec
-
 object Duplicates {
   /**
    * Input is list of integers. All numbers in the list appear exactly twice, except one number.
    * Returning this one number.
    */
   def findNotDuplicatedNumber(list: List[Int]): Int = {
-    @tailrec
-    def tailRec(todo: List[Int], uniques: Set[Int]): Int = {
-      if (todo.isEmpty) uniques.head
-      else {
-        val head = todo.head
-        if (uniques.contains(head)) tailRec(todo.tail, uniques - head)
-        else tailRec(todo.tail, uniques + head)
-      }
-    }
+    /*
+    ^ - XOR
+    If a is Int, then a^a=0
+    0^a=a
+     */
 
-    tailRec(list, Set())
+    list.foldLeft(0)(_ ^ _)
   }
 
 }
