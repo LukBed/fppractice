ab5cb1276cb455490fe6456697c9d8f0b1172a47
Lukasz Bednorz <lbednorz.sag@gmail.com>
2021-05-06 10:36:52 +0200

    town judge using foldLeft with syntax sugar

--- src/main/scala/pl/snipersoft/fppractice/graphs/TownJudge.scala
+++ src/main/scala/pl/snipersoft/fppractice/graphs/TownJudge.scala
@@ -14,21 +14,18 @@ object TownJudge {
    */
   def findJudge(n: Int, trust: List[Trust]): Option[Int] = {
 
-    val outDegrees: Map[Int, Int] = trust.foldLeft(Map[Int, Int]())((map, pair) => {
-      val trusting = pair._1
-      val newTrusted: Int = map.getOrElse(trusting, 0)+1
-      map + (trusting -> newTrusted)
-    })
+    val outDegrees: Map[Int, Int] = trust.foldLeft(Map[Int, Int]()) {
+      case (map, (trusting, _)) => map + (trusting -> (1 + map.getOrElse(trusting, 0)))
+    }
 
-    val inDegrees: Map[Int, Int] = trust.foldLeft(Map[Int, Int]())((map, pair) => {
-      val trusted = pair._2
-      val newTrusting: Int = map.getOrElse(trusted, 0)+1
-      map + (trusted -> newTrusting)
-    })
+    val inDegrees: Map[Int, Int] = trust.foldLeft(Map[Int, Int]()) {
+      case (map, (_, trusted)) => map + (trusted -> (1 + map.getOrElse(trusted, 0)))
+    }
 
     def isDistrustful(maybeJudge: Int): Boolean = outDegrees.getOrElse(maybeJudge, 0) == 0
+
     def isTrustedByEveryoneElse(maybeJudge: Int): Boolean = inDegrees.get(maybeJudge).contains(n - 1)
 
-    Range(1, n+1).find(maybeJudge => isDistrustful(maybeJudge) && isTrustedByEveryoneElse(maybeJudge))
+    (1 to n).find(maybeJudge => isDistrustful(maybeJudge) && isTrustedByEveryoneElse(maybeJudge))
   }
 }
