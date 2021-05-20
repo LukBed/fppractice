0882cc195be478ab7f20b4346ea86f0ab1ded96b
Lukasz Bednorz <lbednorz.sag@gmail.com>
2021-05-20 21:29:04 +0200

    type classes pattern

--- src/main/scala/pl/snipersoft/advanced/implicits/TypeClasses.scala
+++ src/main/scala/pl/snipersoft/advanced/implicits/TypeClasses.scala
@@ -2,7 +2,7 @@ package pl.snipersoft.advanced.implicits
 
 object TypeClasses extends App {
   intro()
-  exercise()
+  exerciseEqual()
 
   case class User(name: String, age: Int, mail: String)
 
@@ -15,26 +15,51 @@ object TypeClasses extends App {
       def serialize(value: T): String
     }
 
+    object HTMLSerializer {
+      def serialize[T](value: T)(implicit serializer: HTMLSerializer[T]): String = serializer.serialize(value)
+      def apply[T](implicit serializer: HTMLSerializer[T]): HTMLSerializer[T] = serializer
+    }
+
     //type class instance
-    object UserHTMLSerializer extends HTMLSerializer[User] {
+    implicit object UserHTMLSerializer extends HTMLSerializer[User] {
       override def serialize(u: User): String = s"<div>${u.name}, ${u.age} (<i>${u.mail}</i>)</div>"
     }
 
-    println(UserHTMLSerializer.serialize(janek))
-  }
-
-  def exercise(): Unit = {
-    trait Equal[T] {
-      def apply(o1: T, o2: T): Boolean
+    implicit object IntSerializer extends HTMLSerializer[Int] {
+      override def serialize(value: Int): String = s"<div><i>$value</i></div>"
     }
 
-    object UsersNameEqual extends Equal[User] {
-      override def apply(o1: User, o2: User): Boolean = o1.name == o2.name
+    println(HTMLSerializer.serialize(janek))
+    println(HTMLSerializer.serialize(45))
+
+    //access to entire type class methods
+    println(HTMLSerializer[User].serialize(janek))
+  }
+
+  def exerciseEqual(): Unit = {
+    trait Equal[T] {
+      def isEqual(o1: T, o2: T): Boolean
+    }
+
+    object Equal {
+      def apply[T](implicit equal: Equal[T]): Equal[T] = equal
+      def apply[T](o1: T, o2: T)(implicit equalizer: Equal[T]): Boolean = equalizer.isEqual(o1, o2)
+    }
+
+    implicit object UsersNameEqual extends Equal[User] {
+      override def isEqual(o1: User, o2: User): Boolean = o1.name == o2.name
     }
 
     object UsersNameAndAgeEqual extends Equal[User] {
-      override def apply(o1: User, o2: User): Boolean = o1.name == o2.name && o1.age == o2.age
+      override def isEqual(o1: User, o2: User): Boolean = o1.name == o2.name && o1.age == o2.age
     }
+
+    val u1 = User("Janek", 25, "janek@janek.pl")
+    val u2 = User("Marek", 25, "marek@janek.pl")
+    println(Equal[User].isEqual(u1, u2))
+
+    //ad-hoc polymorphism
+    println(Equal.apply(u1, u2))
   }
 
 
