e38a4deafbafad86b51a5bf137056a8b69e0293b
Lukasz Bednorz <lbednorz.sag@gmail.com>
2021-05-22 08:01:03 +0200

    danger zone of implicit methods

--- src/main/scala/pl/snipersoft/advanced/implicits/ImplicitMethods.scala
+++ src/main/scala/pl/snipersoft/advanced/implicits/ImplicitMethods.scala
@@ -1,20 +1,37 @@
 package pl.snipersoft.advanced.implicits
 
+//avoid this kind of implicits - bugs is hard to find!
 object ImplicitMethods extends App {
-  case class Person(name: String) {
-    def greet = s"Hi, my name is $name"
+
+  introduction()
+  danger()
+
+  def introduction(): Unit = {
+    case class Person(name: String) {
+      def greet = s"Hi, my name is $name"
+    }
+
+    implicit def personFromString(s: String): Person = Person(s)
+
+    val p: Person = "Kurt"
+    println("Janek".greet)
+
+    //if it's more than one possible solution, code will not compile!
+    /*
+    class A {
+      def greet: Int = 2
+    }
+    implicit def aFromString(s: String): A = new A
+    */
   }
 
-  implicit def personFromString(s: String): Person = Person(s)
+  def danger(): Unit = {
+    implicit def isPositive(n: Int): Boolean = n>0
+    //if (n) ...
 
-  val p: Person = "Kurt"
-  println("Janek".greet)
-
-  //if it's more than one possible solution, code will not compile!
-  /*
-  class A {
-    def greet: Int = 2
+    //it works, but our bug in implicit method is very hard to find
+    if (25) println("Ok!")
+    else if (-2) println("Nok")
   }
-  implicit def aFromString(s: String): A = new A
-  */
+
 }
