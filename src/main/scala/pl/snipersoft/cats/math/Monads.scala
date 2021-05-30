68637c6a23c8825efe68e6e162c2b1f6f1825fc4
Lukasz Bednorz <lbednorz.sag@gmail.com>
2021-05-30 08:50:01 +0200

    custom monads

--- src/main/scala/pl/snipersoft/cats/math/Monads.scala
+++ src/main/scala/pl/snipersoft/cats/math/Monads.scala
@@ -8,6 +8,7 @@ import cats.instances.future._
 import cats.instances.either._
 import cats.syntax.monad._
 
+import scala.annotation.tailrec
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.util.{Failure, Success, Try}
@@ -21,6 +22,10 @@ object Monads extends App {
   extensionMethods()
   either()
   generalizationExercise()
+  customMonads()
+  customIdentityMonad()
+  treeMonadStackRecursive()
+  treeMonadTailRecursive()
 
   def myMonad(): Unit = {
     trait MyMonad[M[_]] {
@@ -131,5 +136,103 @@ object Monads extends App {
     testService[Option]
     testService[Future]
     testService[LoadingOr]
+  }
+
+  def customMonads(): Unit = {
+    implicit object OptionMonad extends Monad[Option] {
+      override def pure[A](x: A): Option[A] = Option(x)
+      override def flatMap[A, B](fa: Option[A])(f: A => Option[B]): Option[B] = fa.flatMap(f)
+      //for iteration methods
+      @tailrec
+      override def tailRecM[A, B](a: A)(f: A => Option[Either[A, B]]): Option[B] =
+        f(a) match {
+          case None => None
+          case Some(Left(v)) => tailRecM(v)(f) //if left run this method again with new argument
+          case Some(Right(v)) => Some(v)
+        }
+    }
+  }
+
+  def customIdentityMonad(): Unit = {
+    type Identity[T] = T
+    val n: Identity[Int] = 42
+
+    implicit object IdentityMonad extends Monad[Identity] {
+      override def pure[A](x: A): Identity[A] = x
+      override def flatMap[A, B](fa: Identity[A])(f: A => Identity[B]): Identity[B] = f(fa)
+      @tailrec
+      override def tailRecM[A, B](a: A)(f: A => Identity[Either[A, B]]): Identity[B] = f(a) match {
+        case Left(v) => tailRecM(v)(f)
+        case Right(v) => v
+      }
+    }
+
+    println(n.flatMap(x => x+10))
+  }
+
+  def treeMonadStackRecursive(): Unit = {
+    sealed trait Tree[+A]
+    final case class Leaf[+A](a: A) extends Tree[A]
+    final case class Branch[+A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+    implicit object TreeMonad extends Monad[Tree] {
+      override def pure[A](x: A): Tree[A] = Leaf(x)
+      override def flatMap[A, B](a: Tree[A])(f: A => Tree[B]): Tree[B] = a match {
+        case Leaf(v) => f(v)
+        case Branch(l, r) => Branch(flatMap(l)(f), flatMap(r)(f))
+      }
+      override def tailRecM[A, B](a: A)(f: A => Tree[Either[A, B]]): Tree[B] = {
+        def stackRec(t: Tree[Either[A, B]]): Tree[B] = t match {
+          case Leaf(Left(v)) => stackRec(f(v))
+          case Leaf(Right(v)) => Leaf(v)
+          case Branch(l, r) => Branch(stackRec(l), stackRec(r))
+        }
+        stackRec(f(a))
+      }
+    }
+
+    val tree: Tree[Int] = Branch(Leaf(1), Branch(Leaf(2), Leaf(3)))
+    val newTree = TreeMonad.flatMap(tree)(v => Branch(Leaf(v), Leaf(10*v)))
+    println(newTree)
+  }
+
+  def treeMonadTailRecursive(): Unit = {
+    sealed trait Tree[+A]
+    final case class Leaf[+A](a: A) extends Tree[A]
+    final case class Branch[+A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+    implicit object TreeMonad extends Monad[Tree] {
+      override def pure[A](x: A): Tree[A] = Leaf(x)
+      override def flatMap[A, B](a: Tree[A])(f: A => Tree[B]): Tree[B] = a match {
+        case Leaf(v) => f(v)
+        case Branch(l, r) => Branch(flatMap(l)(f), flatMap(r)(f))
+      }
+      override def tailRecM[A, B](a: A)(f: A => Tree[Either[A, B]]): Tree[B] = {
+        @tailrec
+        def tailRec(todo: List[Tree[Either[A, B]]], expanded: Set[Tree[Either[A, B]]],
+                    done: List[Tree[B]]): Tree[B] = {
+          if (todo.isEmpty) done.head
+          else todo.head match {
+            case Leaf(Left(v)) => tailRec(f(v) :: todo.tail, expanded, done)
+            case Leaf(Right(b)) => tailRec(todo.tail, expanded, Leaf(b) :: done)
+            case node @ Branch(left, right) =>
+              if (!expanded.contains(node)) tailRec(right :: left :: todo, expanded + node, done)
+              else {
+                val newLeft = done.head
+                val newRight = done.tail.head
+                val newBranch = Branch(newLeft, newRight)
+                tailRec(todo.tail, expanded, newBranch :: done.drop(2))
+              }
+          }
+
+        }
+
+        tailRec(List(f(a)), Set(), List())
+      }
+    }
+
+    val tree: Tree[Int] = Branch(Leaf(1), Branch(Leaf(2), Leaf(3)))
+    val newTree = TreeMonad.flatMap(tree)(v => Branch(Leaf(v), Leaf(10*v)))
+    println(newTree)
   }
 }
